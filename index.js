741319de08a88048ddb2ac6af84a050e1299dfe3
emilkrebs <onlime@gmx.de>
2022-02-17 17:03:03 +0100

    Custom messages

--- index.js
+++ index.js
@@ -1,25 +1,66 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const colors = require('colors');
+const { createAudioPlayer, joinVoiceChannel, createAudioResource, StreamType } = require('@discordjs/voice');
+
+const { token, playCommand, pauseCommand, resumeCommand, pauseAnswer, playAnswer, resumeAnswer } = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const player = createAudioPlayer();
+
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log(getTime() + colors.green('Bot is Ready.'));
+	console.log(getTime()+ colors.green(`Bot is logged in as ${colors.yellow(client.user.tag)}.`));
 });
+
+function playSound(){
+	let resource = createAudioResource("https://www.myinstants.com/media/sounds/super-idol.mp3", {
+		inputType: StreamType.Arbitrary
+	});
+	player.play(resource);
+}
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
+	if (!interaction.isCommand() && !interaction.guildId) return;
 	const { commandName } = interaction;
+	
+	if (commandName === playCommand) {
+		const channel = interaction.member.voice.channel;
+		if(!channel) return await interaction.reply({ content: "You are not in a voice channel.", ephemeral: true });
+		const connection = joinVoiceChannel({
+			channelId: channel.id,
+			guildId: channel.guild.id,
+			adapterCreator: interaction.guild.voiceAdapterCreator,
+		});
 
-	if (commandName === 'superidol') {
-		await interaction.reply('Pong!');
+		connection.subscribe(player);
+		playSound();
+		player.on('idle', () =>{
+			playSound();
+		})
+		player.unpause();
+		console.log(getTime() + colors.green('Bot is playes sound.'));
+		return await interaction.reply({ content: playAnswer, ephemeral: false });
 	}
+	else if (interaction.commandName === pauseCommand) {
+		player.pause();
+		console.log(getTime() + colors.green('Bot is paused sound.'));
+		return await interaction.reply({ content: pauseAnswer, ephemeral: false });
+	}
+	else if (interaction.commandName === resumeCommand) {
+		player.pause();
+		console.log(getTime() + colors.green('Bot is unpaused sound.'));
+		return await interaction.reply({ content: resumeAnswer, ephemeral: false });
+	}
+
 });
 
-
+function getTime() {
+    let dateTime = new Date()
+    return colors.gray(`[${dateTime.getHours()}:${dateTime.getMinutes()}:${dateTime.getSeconds()}] `);
+}
 // Login to Discord with your client's token
 client.login(token);
